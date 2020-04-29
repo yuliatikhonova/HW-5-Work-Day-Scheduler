da7ca57b77a54ece2c4852bc0ba5e50384368a02
yulia tikhonova <yuliatikhonova@outlook.com>
2020-04-28 17:14:35 -0700

    Added the date using the moment API

---
+++ Assets/script.js
@@ -0,0 +1,13 @@
+//-------------------------------------------------------------------- Variables
+
+
+//-------------------------------------------------------------------- Display Date
+    // Grabbed the moment format from the api,
+var date = moment().format('dddd, MMM Do YY');
+console.log(date);
+
+    //Added it to the html using the id currentDate
+$('#currentDay').append(date)
+
+//-------------------------------------------------------------------- Inside the container
+    // .text area, .description, .time-block, .row, .hour, .past, .present, .future, .saveBtn, 9am to 5pm (hourly)
