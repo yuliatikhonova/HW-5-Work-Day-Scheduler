3ef1248338b7461013f18cffb4c26464ce3f19ba
yulia tikhonova <yuliatikhonova@outlook.com>
2020-05-02 18:27:57 -0700

    adding the trial js of getting the text boxs to change color

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,76 @@
-//-------------------------------------------------------------------- Variables
+$(document).ready(function () { //loads html before loading the javascript
+
+    //-------------------------------------------------------------------- Variables
+
+    var date = moment().format('dddd, MMM Do YYYY');// Grabbed the day, month, date, and year from the api
+    var time = moment().format('h a');// Grabbed the time from the api
+    
+    var timeLabel = $('.time-block');
+    var textArea = $('.description');
+    var timeArray = ['9 am', '10 am', '11am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm'];
+
+    var saveButton = $('.saveButton');
+
+    //------------------- Display Date ------------------------------------------------//
+    console.log(date);
+
+    $('#currentDay').append(date);//Added it to the html using the id currentDate
+
+    //------------------- Color code past, present, future ---------------------------//
+
+    console.log(time)
+    function timeCheck() {
+        if (time < timeLabel) {
+            textArea.attr("class", "past");
+        };
+        if (time = timeLabel) {
+            textArea.attr("class", "present")
+        };
+        if (time > timeLabel) {
+            textArea.attr("class", "future")
+        };
+    }
 
 
-//-------------------------------------------------------------------- Display Date
-    // Grabbed the moment format from the api,
-var date = moment().format('dddd, MMM Do YYYY');
-console.log(date);
-
-    //Added it to the html using the id currentDate
-$('#currentDay').append(date)
-
-//-------------------------------------------------------------------- Inside the container
-    // .description, .time-block, .past, .present, .future, .saveBtn, 9am to 5pm (hourly)
 
 
+    function addAndCheckTime () {
+        $('#9').append(timeArray[0]);
+        $('#10').append(timeArray[1]);
+        $('#11').append(timeArray[2]);
+        $('#12').append(timeArray[3]);
+        $('#1').append(timeArray[4]);
+        $('#2').append(timeArray[5]);
+        $('#3').append(timeArray[6]);
+        $('#4').append(timeArray[7]);
+        $('#5').append(timeArray[8]);
+
+        for (var index = 0; i < timeArray.length; i++) {
+            timeCheck();   
+        }
+    }
+    
+    addAndCheckTime();
+
+    //-----------------
+
+
+
+
+
+
+
+
+
+    // function saveButtonClicked(){
+    //     $(saveButton).on("click", function() {
+
+    //     });
+    //     saveToLocalStorage()
+
+    // }
+
+
+
+
+});
--- README.md
+++ README.md
@@ -17,10 +17,10 @@ SO THAT I can manage my time effectively
 ```
 GIVEN I am using a daily planner to create a schedule
 
-WHEN I open the planner
+    WHEN I open the planner
     THEN the current day is displayed at the top of the calendar
 
-WHEN I scroll down
+    WHEN I scroll down
     THEN I am presented with timeblocks for standard business hours
 
 WHEN I view the timeblocks for that day
